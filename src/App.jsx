da64bea46ebc6d6670280c8c5780026f173ef89d
hackerboy09 <isc1930319@gmail.com>
2023-03-27 09:57:41 -0600

    Configuraciones tecnicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
+
+import { useState } from "react";
 function App() {
-  const [inputValue, setInputValue] = useState("")
-  const handleIInputChange = (e) => {
-    setInputValue(e.target.value);
-    console.log(e.target.value);
+  const [inputValue, setInputValue] = useState({
+    input1: "",
+    input2: "",
+  });
+
+  const handleInputChange = (e) => {
+  setInputValue ({
+    ...inputValue,
+    [e.target.name]: e.target.value,
+  });
   }
 
 return(
   <div style={{textAlign: "center"}}>
   <h1>Input Test</h1>
   <hr />
+  <label htmlFor="">Input1</label>
   <input
-  onChange={(e)=>handleIInputChange(e)}
+  id="in1"
+  name="input1"
+  onChange={(e)=>handleInputChange(e)}
   type="text"
-  value={inputValue} 
+  value={inputValue.input1} 
    />
+  <hr />
+  <label>
+    Input2
+   <input
+   className="mt-2"
+  name="input2"
+  onChange={(e)=>handleInputChange(e)}
+  placeholder='Este es el input2'
+  type="text"
+  value={inputValue.input2} 
+   />
+   </label>
   </div>
 )
 
